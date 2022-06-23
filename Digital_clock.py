9ea48df42fcad60e3152e198238136a58e01aa74
jeshna167 <99404034+jeshna167@users.noreply.github.com>
2022-06-23 17:30:34 +0530

    Update Digital_clock.py

--- Digital_clock.py
+++ Digital_clock.py
@@ -2,13 +2,18 @@ from time import strftime
 #t=strftime("%H %M  %S  %p (am, pm)  %a (weekday) %b (month) %Y (year)  %X (all)")
 #print(t)
 from tkinter import *
+from tkinter.messagebox import showinfo
+from turtle import showturtle
 root=Tk()
 root.title("Digital Clock")
 root.geometry("800x800+50+50")
 root.resizable(0,0)
 def clock():
     # tick=strftime("%H:%M:%S %p %a %b %Y")
-    tick=strftime("%H:%M:%S %p %A %B %d, %Y")
+    #tick=strftime("%H:%M:%S %p %A %B %d, %Y")
+    tick=strftime("%X %p")
+    if tick=="17:27:00 PM":
+        showinfo("alarm","wake up")
     label1.config(text=tick)
     label1.after(1000,clock)
 label=Label(root,font=("arial",50),background="blue",text="time",foreground="white")
